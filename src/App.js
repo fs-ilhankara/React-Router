779c339bee2732eb1cc32935cf1469dc01731c7e
mirackarakasli <ilhansamikara@gmail.com>
2021-08-28 16:22:19 +0300

    react-router add pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import './App.css'
 import Header from './components/Header'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Profile from './pages/Profile'
 
 const App = () => {
   return (
@@ -9,8 +12,14 @@ const App = () => {
       <div className="App">
         <Header />
       </div>
+      <Switch>
+        <Route path='/About' component={About}/>
+        <Route path='/Profile' component={Profile}/>
+        <Route path='/' component={Home}/>
+      </Switch>
     </Router>
-  )}
+  );
+}
 
 export default App
 
