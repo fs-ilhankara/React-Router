8bc3fda6ad746006d8afb901d2e214321fa24adf
mirackarakasli <ilhansamikara@gmail.com>
2021-08-28 16:54:54 +0300

    React-Router ders 2

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Profile from './pages/Profile'
+import NotFound from './pages/NotFound'
 
 const App = () => {
   return (
-    <Router>
+    <Router forceRefresh>
       <div className="App">
         <Header />
       </div>
       <Switch>
-        <Route path='/About' component={About}/>
-        <Route path='/Profile' component={Profile}/>
-        <Route path='/' component={Home}/>
+        <Route  path='/About' component={About}/>
+        <Route  path='/Profile' component={Profile}/>
+        <Route exact path='/' component={Home}/>
+        <Route component={NotFound}/>
       </Switch>
     </Router>
   );
